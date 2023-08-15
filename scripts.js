ca2c9045f05e1793b5af2fb286b0781d991a6baa
khomotso-gojela <131772153+khomotso-gojela@users.noreply.github.com>
2023-08-15 10:50:57 +0200

    removed spaces using the trim method

--- scripts.js
+++ scripts.js
@@ -70,7 +70,7 @@ const leo = {
 };
 
 const sarah = {
-	name: sarahName + sarahSurname,
+	name: `${sarahName.trim()} ${sarahSurname}`,
 	age: 62,
 	access_id: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
 	balance: sarahBalance,
